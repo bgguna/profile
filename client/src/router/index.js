25222030b72ffd17d8c8d5a142316ea0405effc3
Bogdan Guna <bogdan.guna@gmail.com>
2020-07-20 18:10:33 +0100

    feat(me): add routing * added about-me view with component * added contact-me view with component

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,27 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import AboutMe from '@/views/AboutMe.vue'
+import ContactMe from '@/views/ContactMe.vue'
 
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    component: AboutMe
   },
   {
     path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: AboutMe
+  },
+  {
+    path: '/contact',
+    component: ContactMe
   }
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode: 'history'
 })
 
 export default router
---
+++ client/src/views/ContactMe.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="contact">
+    <Contact msg="Please reach out!"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Contact from '@/components/Contact.vue'
+
+export default {
+  name: 'ContactMe',
+  components: {
+    Contact
+  }
+}
+</script>
